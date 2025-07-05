d1425a087ce53d933bcd59d6252527ff34edcd8f
Ismai21k <ismailkabir170@gmail.com>
2025-07-05 06:13:22 +0100

    add backend url

--- frontend/src/Api.jsx
+++ frontend/src/Api.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const API = axios.create({
-    baseURL:'http://localhost:5000',
+    baseURL:['http://localhost:5000','https://task-manager-3-22fc.onrender.com'][1],//
 });
